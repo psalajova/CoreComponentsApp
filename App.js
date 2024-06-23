fd2343ada4c3884e9c042226a8b974c00b1bae9c
pvaia <salajova.p@gmail.com>
2024-06-23 08:13:02 +0200

    Add scroll view

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return <View style={{
     flex: 1, // flex to take up all the available screen
     backgroundColor: "plum",
-    padding: 60,
+    padding: 50,
   }}>
-    <View style={{
-      width: 200,
-      height: 200,
-      backgroundColor: "white",
-    }}></View>
-    <Text>
-      <Text style={{ color: "white", }}> Hi!fghfhgfhfhfh</Text>
-      Hello worlddd
-    </Text>
+    <ScrollView>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id hendrerit sem,
+        nec suscipit augue. Quisque interdum nibh tellus. In sodales lorem quis consequat iaculis.
+        Aenean ut erat porta, feugiat neque in, feugiat nunc. Donec eu leo dui. Ut lobortis mollis nibh,
+        eu hendrerit est mollis eget. Vivamus tempor arcu id metus efficitur iaculis.
 
-    <ImageBackground source={logoImg} style={{ flex: 1, }}>
-      <Image source={"https://picsum.photos/200"} style={{ width: 300, height: 300 }} />
-      <Text>BG IMAGE TEXT</Text>
-    </ImageBackground>
+        Fusce sagittis porta nunc in pulvinar. Curabitur elit justo, sodales quis cursus a,
+        aliquet non est. Vivamus aliquet, nulla vel sagittis tempus, ligula nulla fermentum
+        lacus, non congue ligula tortor ac mi. Mauris non eros facilisis, lacinia sem quis,
+        dignissim leo. Donec bibendum enim quis nibh elementum condimentum. Aliquam congue
+        turpis ac scelerisque congue. Cras in fermentum dolor. Nullam ultrices ex non justo
+        tempor, vel ultrices felis luctus. Nam pharetra iaculis augue in posuere. Aliquam vel
+        augue ligula. Nunc aliquam turpis lacus, pharetra laoreet leo malesuada et.
+      </Text>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    </ScrollView>
   </View>;
 }
 
