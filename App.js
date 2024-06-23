944158b51f110471d86941aaa9cee48a7f3e524c
pvaia <salajova.p@gmail.com>
2024-06-23 08:05:06 +0200

    View, Text, Image

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={{
+    flex: 1, // flex to take up all the available screen
+    backgroundColor: "plum",
+    padding: 60,
+  }}>
+    <View style={{
+      width: 200,
+      height: 200,
+      backgroundColor: "white",
+    }}></View>
+    <Text>
+      <Text style={{ color: "white", }}> Hi!fghfhgfhfhfh</Text>
+      Hello worlddd
+    </Text>
+
+    <ImageBackground source={logoImg} style={{ flex: 1, }}>
+      <Image source={"https://picsum.photos/200"} style={{ width: 300, height: 300 }} />
+      <Text>BG IMAGE TEXT</Text>
+    </ImageBackground>
+  </View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
