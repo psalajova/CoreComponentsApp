06ab7c5c3b1bd74cfcbdeadada0f74c7e1b8178f
pvaia <salajova.p@gmail.com>
2024-06-24 17:22:14 +0200

    Add ActivityIndicator

--- App.js
+++ App.js
@@ -1,36 +1,15 @@
-import { useState } from "react";
-import { Button, StatusBar, View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 
 export default function App() {
-  [statusBarHidden, setStatusBarHidden] = useState(false)
-
   return <View style={{
     flex: 1, // flex to take up all the available screen
     backgroundColor: "plum",
     padding: 50,
   }}>
-    <StatusBar
-      backgroundColor={"white"} //only shows up on Android
-      barStyle={"default"} //changes the color of text on status bar (time, battery, etc)
-      hidden={statusBarHidden} //hides status bar
-    />
-    <Button
-      title="hide bar"
-      onPress={() => {
-        if (statusBarHidden == false) {
-          setStatusBarHidden(true);
-        }
-      }}>
-    </Button>
-
-    <Button
-      title="show bar"
-      onPress={() => {
-        if (statusBarHidden == true) {
-          setStatusBarHidden(false);
-        }
-      }}>
-    </Button>
+    <ActivityIndicator />
+    <ActivityIndicator size={"large"} />
+    <ActivityIndicator size={"large"} color={"red"} />
+    <ActivityIndicator size={"large"} color={"white"} animating={false} />
   </View>;
 }
 
