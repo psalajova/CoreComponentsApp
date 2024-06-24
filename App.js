8ebfe8722bf1565b9b40817a37e9db1b2d51a5e5
pvaia <salajova.p@gmail.com>
2024-06-24 17:18:25 +0200

    StatusBar

--- App.js
+++ App.js
@@ -1,84 +1,36 @@
-import { Button, View, Image, Pressable, Text, Modal } from "react-native";
 import { useState } from "react";
-
-const logoImg = require("./assets/adaptive-icon.png")
+import { Button, StatusBar, View } from "react-native";
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  [statusBarHidden, setStatusBarHidden] = useState(false)
 
   return <View style={{
     flex: 1, // flex to take up all the available screen
     backgroundColor: "plum",
     padding: 50,
   }}>
+    <StatusBar
+      backgroundColor={"white"} //only shows up on Android
+      barStyle={"default"} //changes the color of text on status bar (time, battery, etc)
+      hidden={statusBarHidden} //hides status bar
+    />
     <Button
-      title="Press ME"
-      onPress={(event) => {
-        console.log("Button pressed")
-        console.log(event.target.title)
-      }} />
-
-    {/* Pressable: wrapper component that makes other components pressable
-
-    We can create a custom button with it:
-    onPressIn   - called when press is activated
-    onLongPress - called when press is held for more than 500 ms
-    onPressOut  - called when press is deactivated */}
-    <Pressable onPress={() => {
-      console.log("Image pressed!");
-    }}>
-      <Image source={logoImg} style={{ height: 300, width: 300 }} />
-    </Pressable>
-
-    <Pressable onPress={() => {
-      console.log("Text pressed!");
-    }}>
-      <Text
-        style={{
-          fontSize: 20,
-          textAlign: "center",
-          backgroundColor: '#1E6738',
-          color: "white",
-          padding: 20,
-        }}
-        onPress={() => {
-          setIsModalVisible(true);
-          console.log("opening modeal");
-        }}>
-        Pressable text here - opens modal
-      </Text>
-    </Pressable>
-
-    <Modal
-      visible={isModalVisible}
-      onRequestClose={() => { // closes the modal if the user uses the back button on Android/closing gesture on iOS
-        setIsModalVisible(false);
-      }}
-      animationType="slide" //or fade
-      presentationStyle="formSheet" //set to 'fullscreen' by default, other: "formSheet"
-    >
-
-      <View style={{
-        flex: 1, // flex to take up all the available screen
-        backgroundColor: "lightblue",
-        padding: 50,
+      title="hide bar"
+      onPress={() => {
+        if (statusBarHidden == false) {
+          setStatusBarHidden(true);
+        }
       }}>
-        <Text>
-          Modal content.
-          Modal content.
-          Modal content.
-          Modal content.
-        </Text>
-        <Button
-          title="Close this modal"
-          color="midnightblue"
-          onPress={() => {
-            setIsModalVisible(false);
-            console.log("closing modal");
-          }} />
-      </View>
-    </Modal>
+    </Button>
 
+    <Button
+      title="show bar"
+      onPress={() => {
+        if (statusBarHidden == true) {
+          setStatusBarHidden(false);
+        }
+      }}>
+    </Button>
   </View>;
 }
 
