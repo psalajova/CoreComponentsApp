22c689108a0e5ef4f995d05fc86e50094347de1c
Patricia Salajova <salajova.p@gmail.com>
2024-07-02 20:19:41 +0200

    Add basic structure

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: "plum",
+      padding: 60
+    }}>
+      <Text>StyleSheet API</Text>
+    </View>
+  )
+}
