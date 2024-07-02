99ec48c70f2c464bbbc03d78fdabf2296ddac1b0
Patricia Salajova <salajova.p@gmail.com>
2024-07-02 20:22:20 +0200

    Add title

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "plum",
-      padding: 60
-    }}>
-      <Text>StyleSheet API</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 60
+  },
+  title: {
+    color: "green",
+  }
+})
