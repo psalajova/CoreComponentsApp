439ab7e346667d1f1239e0c7536bdc6feaa1fb2e
Patricia Salajova <salajova.p@gmail.com>
2024-07-06 10:36:08 +0200

    Add shadows

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.blueBg]}>
+      <View style={[styles.box, styles.blueBg, styles.boxShadow]}>
         <Text style={styles.cellText}>StyleSheet API</Text>
       </View>
 
-      <View style={[styles.box, styles.greenBg]}>
-        <Text>StyleSheet API</Text>
+      <View style={[styles.box, styles.peachBg, styles.boxShadow, styles.androidShadow]}>
+        <Text style={styles.cellText}>StyleSheet API</Text>
       </View>
     </View>
   )
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#95bda0",
+    backgroundColor: "white",
     padding: 60,
     alignItems: 'center',
   },
@@ -31,19 +31,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',     //center text horizontally
     justifyContent: 'center', //center text vertically
     //border:
-    borderWidth: 2,
-    borderStyle: "dotted",
-    borderColor: "#16243b",
+    // borderWidth: 2,
+    // borderStyle: "solid",
+    // borderColor: "#16243b",
     borderRadius: 10, //only applies to View - when applied to Text, doesn't show on iOS, only Android
 
   },
   blueBg: {
-    backgroundColor: "lightblue",
+    backgroundColor: "#c7e0f2",
   },
-  greenBg: {
-    backgroundColor: "lightgreen",
+  peachBg: {
+    backgroundColor: "#d1a699",
   },
   cellText: {
     fontFamily: 'Georgia',
   },
+  boxShadow: { // only shows up in ioS
+    shadowColor: "black",
+    shadowOffset: {
+      width: 4,
+      height: 2,
+    },
+    shadowOpacity: 0.15, // 0 - transparent, 1 - opaque
+    shadowRadius: 5,
+  },
+  androidShadow: {
+    elevation: 10,
+  }
 })
